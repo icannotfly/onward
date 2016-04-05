3f90c31784533cfda66e97aaae96be61115d2949
icannotfly <tgharwe@gmail.com>
2016-04-04 21:48:49 -0700

    initial commit

---
+++ onwardWorldSettings.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/WorldSettings.h"
+#include "onwardWorldSettings.generated.h"
+
+
+
+UCLASS()
+class ONWARD_API AonwardWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
