262b2fd14632adfb13fc50afd45d5214f363624d
icannotfly <icannotfly@icannotfly.net>
2016-04-09 21:25:23 -0700

    added and rephrased some defines

--- onward.h
+++ onward.h
@@ -7,9 +7,18 @@
 
 
 
-#define GAME_HOURS_PER_DAY 24.0
-#define GAME_MINUTES_PER_HOUR 60.0
-#define GAME_SECONDS_PER_MINUTE 60.0
+#define GAME_MONTHS_IN_YEAR 8.0
+#define GAME_DAYS_IN_MONTH 20.0
+#define GAME_HOURS_IN_DAY 24.0
+#define GAME_MINUTES_IN_HOUR 60.0
+#define GAME_SECONDS_IN_MINUTE 60.0
+
+//these values MUST be recalculated if any of the above values change
+#define GAME_SECONDS_IN_YEAR 13824000.0
+#define GAME_SECONDS_IN_MONTH 1728000.0
+#define GAME_SECONDS_IN_DAY 86400.0
+#define GAME_SECONDS_IN_HOUR 3600.0
+
 
 
 
