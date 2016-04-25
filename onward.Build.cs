6e417a5026aafab9e4310b20a591b38a40d62bbe
icannotfly <icannotfly@icannotfly.net>
2016-04-24 20:44:43 -0700

    added dependencies for slate

--- onward.Build.cs
+++ onward.Build.cs
@@ -7,5 +7,6 @@ public class onward : ModuleRules
 	public onward(TargetInfo Target)
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 	}
 }
