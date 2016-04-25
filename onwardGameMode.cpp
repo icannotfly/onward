3a6830f3eaf555ae2e1e8e884df21736268c338d
icannotfly <icannotfly@icannotfly.net>
2016-04-24 20:44:22 -0700

    added TimeRate console command

--- onwardGameMode.cpp
+++ onwardGameMode.cpp
@@ -4,6 +4,7 @@
 #include "onwardCharacter.h"
 #include "onwardGameInstance.h"
 #include "onwardWorldSettings.h"
+#include "onwardHUD.h"
 #include "onwardGameMode.h"
 
 AonwardGameMode::AonwardGameMode()
@@ -18,7 +19,12 @@ AonwardGameMode::AonwardGameMode()
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}	
+	}
+
+
+
+	//Set the hud class to use our custom HUD by default.
+	this->HUDClass = AonwardHUD::StaticClass();
 }
 
 
@@ -42,4 +48,20 @@ void AonwardGameMode::Tick(float DeltaSeconds)
 
 
 	Super::Tick(DeltaSeconds);
+}
+
+
+
+void AonwardGameMode::TimeRate(float iNewRate)
+{
+	AonwardWorldSettings* WS = Cast<AonwardWorldSettings>(GetWorldSettings());
+
+	if (WS)
+	{
+		WS->SetRealWorldMinutesPerInGameDay(iNewRate); //validation is done in here
+	}
+	else
+	{
+		UE_LOG(LogWorldTime, Warning, TEXT("Could not change time rate: could not find WorldSettings."));
+	}
 }
--- onwardGameMode.h
+++ onwardGameMode.h
@@ -15,6 +15,11 @@ public:
 	AonwardGameMode();
 
 	virtual void Tick(float DeltaSeconds) override;
+
+
+
+	//sets the rate of time's passage. units passed are real-world minutes per in-game day. valid range is (0, 1440]
+	UFUNCTION(exec) void TimeRate(float iNewRate);
 };
 
 
