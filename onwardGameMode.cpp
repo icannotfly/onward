613bd8cce6cc89438463d139948ff6cdf5dac3f3
icannotfly <icannotfly@icannotfly.net>
2016-05-26 23:04:19 -0700

    added Time, TimeRate, and TimeSet

--- onwardGameMode.cpp
+++ onwardGameMode.cpp
@@ -41,13 +41,36 @@ void AonwardGameMode::Tick(float DeltaSeconds)
 			float SecondsToAdd = DeltaSeconds * WS->GetRealWorldToInGameTimeConversionFactor();
 			GI->GetWorldTime()->AddSeconds(SecondsToAdd);
 
-			UE_LOG(LogWorldTime, Log, TEXT("time: %s  -  added %f sec to world time"), *(GI->GetWorldTime()->ToString(2)), SecondsToAdd);
+			UE_LOG(LogWorldTime, Verbose, TEXT("time: %s  -  added %f sec to world time"), *(GI->GetWorldTime()->ToString(2)), SecondsToAdd);
 		}
 	}
 
 
 
 	Super::Tick(DeltaSeconds);
+}
+
+
+
+void AonwardGameMode::Time(FString iTime)
+{
+	FString t = iTime.ToLower();
+	uint8 TargetHour = 0;
+
+	//set general time of day
+	/*0600*/ if		 (t == "morning")	{ TargetHour = 6; }
+	/*1000*/ else if (t == "day")		{ TargetHour = 10; }
+	/*1200*/ else if (t == "midday")	{ TargetHour = 12; }
+	/*1400*/ else if (t == "afternoon")	{ TargetHour = 14; }
+	/*1800*/ else if (t == "evening")	{ TargetHour = 18; }
+	/*2200*/ else if (t == "night")		{ TargetHour = 22; }
+	/*2400*/ else if (t == "midnight")	{ TargetHour = 0; }
+
+	else
+	{
+		UE_LOG(LogWorldTime, Warning, TEXT("%s::Time(): I don't understand what you mean by %s."), *GetName(), *iTime);
+		return;
+	}
 }
 
 
@@ -60,8 +83,76 @@ void AonwardGameMode::TimeRate(float iNewRate)
 	{
 		WS->SetRealWorldMinutesPerInGameDay(iNewRate); //validation is done in here
 	}
+	
+	UE_LOG(LogWorldTime, Warning, TEXT("Could not change time rate: could not find WorldSettings."));
+}
+
+
+
+void AonwardGameMode::TimeSet(uint32 iYear, uint8 iMonth, uint8 iDay, uint8 iHour, uint8 iMinute, uint8 iSecond)
+{
+	UonwardGameInstance* GI = Cast<UonwardGameInstance>(GetGameInstance());
+
+	if (GI)
+	{
+		FTimestamp TargetTime = FTimestamp(iYear, iMonth, iDay, iHour, iMinute, iSecond);
+
+		if (TargetTime.GetTotalSeconds() > GI->GetWorldTime()->GetTotalSeconds()) //this is all the validation we need
+		{
+			ForwardTimeTo(TargetTime);
+
+			UE_LOG(LogWorldTime, Log, TEXT("changed world time to %s"), *(GI->GetWorldTime()->ToString(1)));
+		}
+		else
+		{
+			UE_LOG(LogWorldTime, Warning, TEXT("Could not set time: could not find GameInstance."));
+		}
+	}
+}
+
+
+
+void AonwardGameMode::ForwardTimeTo(FTimestamp iTargetTime)
+{
+	UonwardGameInstance* GI = Cast<UonwardGameInstance>(GetGameInstance());
+
+	if (GI)
+	{
+		FTimestamp* WorldTime = GI->GetWorldTime();
+
+		//make sure this is indeed in the future
+		if (iTargetTime.GetTotalSeconds() > WorldTime->GetTotalSeconds())
+		{
+			UE_LOG(LogWorldTime, Log, TEXT("Forwarding time to %s"), *(iTargetTime.ToString()));
+
+			while (iTargetTime.GetTotalSeconds() > WorldTime->GetTotalSeconds())
+			{
+				//add random amount between 1 month and 1 year
+				float SecondsToAdd = FMath::FRandRange(GAME_SECONDS_IN_MONTH, GAME_SECONDS_IN_YEAR);
+
+				//overshoot detection
+				if (WorldTime->GetTotalSeconds() + SecondsToAdd > iTargetTime.GetTotalSeconds())
+				{
+					//we overshot; clamp
+					WorldTime->AddSeconds(iTargetTime.GetTotalSeconds() - WorldTime->GetTotalSeconds());
+				}
+				else
+				{
+					WorldTime->AddSeconds(SecondsToAdd);
+				}
+
+				UE_LOG(LogWorldTime, Log, TEXT("    added %f seconds, time is now %s"), SecondsToAdd, *(WorldTime->ToString()));
+			}
+		}
+		else
+		{
+			UE_LOG(LogWorldTime, Warning, TEXT("Could not forward time: cannot go backwards in time."));
+			return;
+		}
+	}
 	else
 	{
-		UE_LOG(LogWorldTime, Warning, TEXT("Could not change time rate: could not find WorldSettings."));
+		UE_LOG(LogWorldTime, Warning, TEXT("Could not forward time: could not find GameInstance."));
+		return;
 	}
 }
--- onwardGameMode.h
+++ onwardGameMode.h
@@ -18,8 +18,17 @@ public:
 
 
 
+	//shorthand way of setting world time
+	UFUNCTION(exec) void Time(FString iTime);
+
 	//sets the rate of time's passage. units passed are real-world minutes per in-game day. valid range is (0, 1440]
 	UFUNCTION(exec) void TimeRate(float iNewRate);
+
+	//set the world time to a specific point in time. not all values need to be supplied, but incoming values will be assumed in descending order starting with year. values do not need to obey upper limits.
+	UFUNCTION(exec) void TimeSet(uint32 iYear, uint8 iMonth, uint8 iDay, uint8 iHour, uint8 iMinute, uint8 iSecond);
+
+	//forward time to the specified point
+	UFUNCTION() void ForwardTimeTo(FTimestamp iTargetTime);
 };
 
 
