d16e72ea0b268a50007239197cb67407776642e0
icannotfly <icannotfly@icannotfly.net>
2016-10-27 21:09:02 -0700

    added a bit more to the error message example

--- Source/onward/onward.h
+++ Source/onward/onward.h
@@ -25,7 +25,7 @@
 
 //thanks, rama!
 //use like:
-// UE_LOG(HelloWorld, Log, TEXT("%s::%s() (%s)"), *(CURR_CLASS), *(CURR_FUNCTION), *(CURR_LINE));
+// UE_LOG(HelloWorld, Log, TEXT("%s::%s() at line (%s): error message"), *(CURR_CLASS), *(CURR_FUNCTION), *(CURR_LINE));
 #define CURR_CLASS FString(__FUNCTION__).Left(FString(__FUNCTION__).Find(TEXT(":"))) //will print current class
 #define CURR_FUNCTION FString(__FUNCTION__).Right(FString(__FUNCTION__).Len() - FString(__FUNCTION__).Find(TEXT("::")) - 2 ) //will print current function
 #define CURR_LINE FString::FromInt(__LINE__)
